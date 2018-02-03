e30c64d4f7aff3d1e444019e582523d746a579f4
Daniel Costa <costa@vixinet.ch>
2018-02-03 18:04:55 +0200

    rating visitation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
+  validates :rating, :inclusion => { :in => 1..5 }
 
   def to_s
     "project #{name}"
