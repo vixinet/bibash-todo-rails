7ae8f244a62b205087a5627610ce172cc8c62813
daniel-costa <costa@vixinet.ch>
2018-02-09 11:25:13 +0200

    training1.8 mid

---
+++ db/migrate/20180209092015_create_labels.rb
@@ -0,0 +1,10 @@
+class CreateLabels < ActiveRecord::Migration[5.1]
+  def change
+    create_table :labels do |t|
+      t.string :label
+      t.string :color
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/factories/labels.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :label do
+    label "MyString"
+    color "MyString"
+  end
+end
