5be4ee2e212745127e58bd58ebb947758b661bba
daniel-costa <costa@vixinet.ch>
2018-02-09 11:17:38 +0200

    training1.7

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -3,4 +3,22 @@ class Milestone < ApplicationRecord
   validates :deadline, presence: true
   
   belongs_to :project
+
+  def active?
+    if project.status == "open"
+      true
+    else 
+      false
+    end
+  end
+
+  def active_second?
+    # This method is the same than active? in its behaviour 
+    project.status == "open"
+  end
+
+  def active_third?
+    # This method is the same than active? in its behaviour 
+    project.open?
+  end
 end
