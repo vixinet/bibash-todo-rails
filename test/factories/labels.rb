b3cefed988d36a709fa65a0aef80021527b8ef46
Daniel Costa <costa@vixinet.ch>
2018-02-19 17:18:23 +0200

    Add tests for Label

--- test/factories/labels.rb
+++ test/factories/labels.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :label do
-    label "MyString"
-    color "MyString"
+    label "Bug"
+    color "#ff0000"
+
+    factory :label_with_projects do
+      transient do
+        list_count 5
+      end
+
+      after(:create) do |label, evaluator|
+        create_list(:project, evaluator.list_count, labels: [label])
+      end
+    end
   end
 end
--- test/models/label_test.rb
+++ test/models/label_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class LabelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    label = build(:label)
+    assert label.valid?
+  end
+
+  test "should require label" do
+    label = build(:label, label: nil)
+    refute label.valid?
+  end
+
+  test "should require color" do
+    label = build(:label, color: nil)
+    refute label.valid?
+  end
+
+  test "should require color with #" do
+    label = build(:label, color: 'FF0000')
+    refute label.valid?
+  end
+
+  test "should require color with 3 chars" do
+    label = build(:label, color: '#FFF')
+    assert label.valid?
+  end
+
+  test "should reject color with 4 chars" do
+    label = build(:label, color: '#FFFF')
+    refute label.valid?
+  end
+
+  test "should require color with 6 chars" do
+    label = build(:label, color: '#FF0000')
+    assert label.valid?
+  end
+
+  test "should have many projects" do
+    label = create(:label_with_projects)
+    assert_equal 5, label.projects.size
+  end
+
+  test "should not be destroyed when label is destroyed" do
+    label = create(:label_with_projects)
+
+    assert_no_difference 'Label.count' do
+      label.projects.first.destroy
+    end
+
+    assert_difference 'Project.count', -1 do
+      label.projects.first.destroy
+    end
+  end
 end
