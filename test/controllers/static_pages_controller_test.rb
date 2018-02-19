828addf7612ae5c4ca343f290a9418af2e0a6e8b
Daniel Costa <costa@vixinet.ch>
2018-02-19 17:19:05 +0200

    Add tests for StaticPages Controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get '/'
+    assert_response :success
+  end
+
+  test "should get privacy" do
+    get '/privacy'
+    assert_response :success
+  end
+
+  test "should get legal" do
+    get '/legal'
+    assert_response :success
+  end
+
+  test "should get legal-terms" do
+    get '/legal-terms'
+    assert_response :success
+  end
+
+  test "should get location" do
+    get '/location'
     assert_response :success
   end
 
