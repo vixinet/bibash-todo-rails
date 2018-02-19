f7adf219832cb628821a2f99463f70cba46295ca
Daniel Costa <costa@vixinet.ch>
2018-02-19 18:03:11 +0200

    bugfix
    Co-Authored-By: Bibash Shah <shahbibash@users.noreply.github.com>

--- db/migrate/20180216134435_remove_description_from_projects.rb
+++ db/migrate/20180216134435_remove_description_from_projects.rb
@@ -1,5 +1,5 @@
 class RemoveDescriptionFromProjects < ActiveRecord::Migration[5.1]
   def change
-    remove_column :projects, :description, :text
+    remove_column :projects, :description
   end
 end
