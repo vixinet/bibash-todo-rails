eba74014744df66b0be247268aefa489b233fb83
Daniel Costa <costa@vixinet.ch>
2018-02-19 17:21:04 +0200

    bis

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -5,20 +5,6 @@ class Milestone < ApplicationRecord
   belongs_to :project
 
   def active?
-    if project.status == "open"
-      true
-    else 
-      false
-    end
-  end
-
-  def active_second?
-    # This method is the same than active? in its behaviour 
-    project.status == "open"
-  end
-
-  def active_third?
-    # This method is the same than active? in its behaviour 
     project.open?
   end
 end
